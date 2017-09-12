a3bdf8c5eb04164b221241e99f042ba18b234d2c
Raphael Neumann <raphael@rneumann.me>
2017-09-12 18:18:35 -0300

    Incluido class params com os parametros necessários

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,32 +4,21 @@ class chrony::params {
   $service_ensure   = 'running'
   $service_manage   = true
   $servers = {
-     '0.pool.ntp.org' => ['iburst'],
-     '1.pool.ntp.org' => ['iburst'],
-     '2.pool.ntp.org' => ['iburst'],
+     'a.ntp.br' => ['iburst'],
+     'b.ntp.br' => ['iburst'],
+     'c.ntp.br' => ['iburst'],
   }
-
 
   case $::osfamily {
     'RedHat' : {
       $config_path = '/etc/chrony.conf'
       $config_template = 'chrony/chrony.conf.redhat.erb'
-      $config_keys = '/etc/chrony.keys'
-      $config_keys_template = 'chrony/chrony.keys.redhat.erb'
-      $config_keys_owner = 0
-      $config_keys_group = chrony
-      $config_keys_mode  = '0640'
       $package_name = 'chrony'
       $service_name = 'chronyd'
     }
     'Debian' : {
       $config_path = '/etc/chrony/chrony.conf'
       $config_template = 'chrony/chrony.conf.debian.erb'
-      $config_keys = '/etc/chrony/chrony.keys'
-      $config_keys_template = 'chrony/chrony.keys.debian.erb'
-      $config_keys_owner = 0
-      $config_keys_group = chrony
-      $config_keys_mode  = '0644'
       $package_name = 'chrony'
       $service_name = 'chrony'
     }
@@ -37,3 +26,4 @@ class chrony::params {
       fail("The ${module_name} module não está preparado sistemas operacionais baseados em ${::osfamily}.")
     }
   }
+}
