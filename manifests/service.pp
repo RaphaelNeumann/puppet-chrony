2bb1aab809e9ffa0fc189db9954d900d20680ca1
Raphael Neumann <raphael@rneumann.me>
2017-09-12 17:47:33 -0300

    Criada classe service para manter o serviço

---
+++ manifests/service.pp
@@ -0,0 +1,16 @@
+class chrony::service (
+  $service_enable = $chrony::service_enable,
+  $service_ensure = $chrony::service_ensure,
+  $service_manage = $chrony::service_manage,
+  $service_name   = $chrony::service_name,
+) inherits chrony {
+  
+  if $service_manage == true {
+    service { 'chrony':
+      ensure     => $service_ensure,
+      enable     => $service_enable,
+      name       => $service_name,
+    }
+  }
+
+}
